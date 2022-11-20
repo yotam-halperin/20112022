43faae6effb33a7259189bb7c53a91e070c7bbe2
yotam_benz <benz.yota@gmail.com>
2022-11-20 13:41:31 +0200

    you are always wrong

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!!!");
         System.out.println("Yotam Halperin,");
-        System.out.println("Yotam Ben Tzvi,");
+        System.out.println("Yotam Ben Zvi,");
         System.out.println("Yotam Other.");
     
     }
