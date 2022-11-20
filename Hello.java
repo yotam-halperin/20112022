03677ebc762df8fe7250fd3c9de00b8006d5bd91
Yuval Klechevsky <yuval.klechvsky27@gmail.com>
2022-11-20 14:02:28 +0200

    new change1

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!!!");
         System.out.println("Yotam Halperin,");
-        System.out.println("Yotam Ben Tzvi,");
+        System.out.println("Yotam Ben Tzvi,1");
         System.out.println("Yotam Other.");
     
     }
