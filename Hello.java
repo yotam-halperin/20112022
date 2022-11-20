8e5f4f78386bb06ee3ec3337037f7abd55eff307
yotam-halperin <yotamhalperin7@gmail.com>
2022-11-20 13:28:59 +0200

    creating first files

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!!!");
+        System.out.println("Yotam Halperin,");
+        System.out.println("Yotam Ben Tzvi,");
+        System.out.println("Yotam Other.");
+    
+    }
+}
